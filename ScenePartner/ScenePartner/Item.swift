e8d85874b50d0863e31dbe6b62579d9085e27a0f
ChrisG <chrisale89@gmail.com>
2026-02-27 21:08:19 -0700

    started Xcode project

---
+++ ScenePartner/ScenePartner/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ScenePartner
+//
+//  Created by Chris Gallegos on 2/27/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ScenePartner/ScenePartnerTests/ScenePartnerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScenePartnerTests.swift
+//  ScenePartnerTests
+//
+//  Created by Chris Gallegos on 2/27/26.
+//
+
+import Testing
+@testable import ScenePartner
+
+struct ScenePartnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
